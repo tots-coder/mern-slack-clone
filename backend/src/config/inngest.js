520c5f434e05121c58acf09a11cda0638b109c28
tots-coder <idevtots@gmail.com>
2025-09-18 18:34:38 +0800

    tiny fixes for inngest.js file

--- backend/src/config/inngest.js
+++ backend/src/config/inngest.js
@@ -31,6 +31,8 @@ const deleteUserFromDB = inngest.createFunction(
   { id: "delete-user-from-db" },
   { event: "clerk/user.deleted" },
   async ({ event }) => {
+    await connectDB();
+
     const { id } = event.data;
     await User.deleteOne({ clerkId: id });
     // TODO: DO MORE THINGS HERE
